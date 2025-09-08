14dd0e32eecdcfc5b0c2dcce7e2feaadf795415d
Shyamala Junjipally <junjipellishyamala005@gmail.com>
2025-09-08 22:37:08 +0530

    Time: 4 ms (1.91%), Space: 15.8 MB (32.98%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-       next_permutation(nums.begin(),nums.end());
-       for(int x:nums)
+    int n = nums.size();
+    int i = n - 2;
+
+    // Step 1: find first index i such that nums[i] < nums[i+1]
+    while (i >= 0 && nums[i] >= nums[i+1]) i--;
+
+    if (i >= 0) {
+        // Step 2: find element just bigger than nums[i]
+        int j = n - 1;
+        while (nums[j] <= nums[i]) j--;
+        swap(nums[i], nums[j]);
+    }
+
+    // Step 3: reverse from i+1 to end
+    reverse(nums.begin() + i + 1, nums.end());for(int x:nums)
        cout<<x;
     }
 };
